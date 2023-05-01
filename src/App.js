36457a64cfa720b2434ddbbbd7e747f41f51512b
rlly92 <reubenlimliangyi@gmail.com>
2023-05-01 16:35:22 +0800

    test 2 for new branch PR

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import logo from "./logo.png";
 import "./App.css";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
@@ -22,3 +22,5 @@ export default App;
 // EXPERIMENTAL CHANGES MADE HERE FOR PRACTICE OF GIT PUSH GIT PULL
 
 // I HAVE ACCIDENTALLY EDITED ON THE MAIN
+
+// THIS IS THE SECOND BRANCH TEST
