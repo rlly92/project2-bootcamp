edbe038c96c83b6ee1fc1207934fe0325ebf24b1
Syafiq Ismail <macamyesjer@gmail.com>
2023-05-01 15:51:22 +0800

    temporary commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import "./App.css";
 
 class App extends React.Component {
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                </header>
-            </div>
-        );
+        return <div className="App"></div>;
     }
 }
 
