220f7725e0f15e8afb17a942901c1bd3fc56ffa3
rlly92 <reubenlimliangyi@gmail.com>
2023-05-06 19:04:39 +0800

    navbar skeletal structure and logout logic and button complete

--- src/components/Sidebar/SidebarWrapper.js
+++ src/components/Sidebar/SidebarWrapper.js
@@ -5,25 +5,6 @@ function SidebarWrapper({ children }) {
     return (
         <Box width={"35%"} p={3} sx={{ overflowY: "auto" }}>
             <Stack
-                direction={"row"}
-                textAlign={"center"}
-                alignItems={"center"}
-                justifyContent={"center"}
-                mb={2}
-            >
-                <Typography variant="h1">bizz</Typography>
-                <Typography
-                    variant="h1"
-                    px={1}
-                    py={0.5}
-                    bgcolor={"black"}
-                    color={"white"}
-                    borderRadius={2}
-                >
-                    bazz
-                </Typography>
-            </Stack>
-            <Stack
                 textAlign={"center"}
                 alignItems={"center"}
                 justifyContent={"center"}
--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -6,22 +6,43 @@ import {
     Stack,
     Button,
 } from "@mui/material";
+import { useNavigate, Outlet } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ handleLogOut }) => {
+    const navigate = useNavigate();
+
+    const handleLogOutAndNavigate = () => {
+        handleLogOut().then(() => navigate("/login"));
+    };
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                {/* <IconButton size='large' edge='start' aria-label='logo' color='inherit'>
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    {/* <IconButton size='large' edge='start' aria-label='logo' color='inherit'>
 
 </IconButton> */}
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    BIZZBAZZ
-                </Typography>
-                <Stack direction="row" spacing={2}>
-                    <Button color="inherit">Log Out</Button>
-                </Stack>
-            </Toolbar>
-        </AppBar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        BIZZBAZZ
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                        <Button
+                            variant="contained"
+                            sx={{ width: "50%", m: 1 }}
+                            onClick={handleLogOutAndNavigate}
+                            type="button"
+                        >
+                            Log Out
+                        </Button>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+            <Outlet />
+        </>
     );
 };
 
