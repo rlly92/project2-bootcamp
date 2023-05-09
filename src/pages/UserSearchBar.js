de129978d25467d87f75beb87a24446d25138685
rlly92 <reubenlimliangyi@gmail.com>
2023-05-09 18:24:54 +0800

    basic UI for user search bar is complete

---
+++ src/pages/UserSearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from "react";
+
+import SearchIcon from "@mui/icons-material/Search";
+import Input from "@mui/material/Input";
+
+function SearchBar() {
+    const [state, setState] = useState({ username: "" });
+
+    const handleChange = (e) => {
+        setState({ ...state.username, [e.target.id]: e.target.value });
+        console.log(state);
+    };
+
+    return (
+        <div>
+            <SearchIcon />
+            <Input
+                value={state.username}
+                id="username"
+                type="username"
+                placeholder="Enter username here to find fellow users"
+                onChange={handleChange}
+            />
+        </div>
+    );
+}
+
+export default SearchBar;
